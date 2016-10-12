fcdcb8aa2e1a146e46f1c0af0a8eddc2a01a8b56
duhamel <duhamel.ramananjafy@gmail.com>
2016-10-12 08:59:01 +0200

    Some improvements
    Do not place a bid if myself is the current winner
    Refresh the page after a bid is placed

--- autobid.js
+++ autobid.js
@@ -4,9 +4,9 @@
  */
 
 /* Constants */
-var BIDING_ZONE = 5;            // If countdown if <= this limit then place a bid
-var MONITORING_ZONE = 10;		// If countdown is <= this limit then check each MONITORING_PERIOD if placing a bid is necessary
-var MONITORING_PERIOD = 1000;	// Time in ms, (cf MONITORING_ZONE desc)
+var BIDING_ZONE = 0;            // If countdown if <= this limit then place a bid
+var MONITORING_ZONE = 5;		// If countdown is <= this limit then check each MONITORING_PERIOD if placing a bid is necessary
+var MONITORING_PERIOD = 300;	// Time in ms, (cf MONITORING_ZONE desc)
 
 /*  Variable Global */
 var intervalID = null
@@ -30,16 +30,21 @@ function _getCountDownValue(b) {
 	return g
 }
 
+function _log(b, msg) {
+	console.log("bid[" + b + "]: " + msg);
+}
+
 /*
  * @Desc Place a bid automatically
  */
 function _autoBid(b) {
 	var count_down = _getCountDownValue(auctionId);
-	console.log(count_down);
+	var winner = jQuery("#Auction_" + b).find(".CurrentWinnerPublicName").html();
+	_log(b, winner + " holds the bid at - " + count_down );
 	if (count_down == null)
 	{
 		// Case: Bid is paused
-		console.log("Not implemented yet");
+		_log(b, "Midnight or sold");
 	}
 	else if (count_down <= MONITORING_ZONE)
 	{
@@ -47,18 +52,20 @@ function _autoBid(b) {
 		if (intervalID == null) {
             intervalID = setInterval(function(){ _autoBid(auctionId) }, MONITORING_PERIOD);
 		}
-		if (count_down <= BIDING_ZONE) {
-		    console.log("Place a bid " + b);
+		if (count_down <= BIDING_ZONE && winner != PublicName) {
 			// Place bid :placeBid(58237)
+		    _log(b, "A bid was placed");
+			window.location.reload();
 		}
 	}
 	else
 	{
 		// Case: Wait until the Watch carefully timezone
-		setTimeout(function(){ console.log("New Check"); }, (count_down - MONITORING_ZONE) * 1000 );
+		var reload_countdown = (count_down - MONITORING_ZONE) * 1000;
+		_log(b, "Reload windows in " + reload_countdown + " ms");
+		setTimeout(function(){ window.location.reload(); }, reload_countdown );
 		clearInterval(intervalID);
 		intervalID = null
-		// Refresh page : window.location.reload()
 	}
 }
 
