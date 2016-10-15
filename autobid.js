8a59d95e5a158d446454a118daaa02d1492ef43f
duhamel <duhamel.ramananjafy@gmail.com>
2016-10-15 18:32:27 +0200

    Code refactoring
    Add some cases : auction hasn't begin + auction is finished
    Improve code comments

--- autobid.js
+++ autobid.js
@@ -1,12 +1,16 @@
 /*
- * All personnal function should be prefixed by "_" to avoid confusion
+ * This code will be append to oopad code on document loading
+ * Functions defined here would be prefixed by "_" to avoid confusion
  * with oopad function
+ *
+ * http://fr.oopad.com/WebServices/BiddingWebService.asmx
  */
 
 /* Constants */
-var BIDING_ZONE = 0;            // If countdown if <= this limit then place a bid
-var MONITORING_ZONE = 5;		// If countdown is <= this limit then check each MONITORING_PERIOD if placing a bid is necessary
-var MONITORING_PERIOD = 300;	// Time in ms, (cf MONITORING_ZONE desc)
+var BIDING_ZONE = -2;       // If countdown <= BIDING_ZONE then place a bid
+var CHECKING_ZONE = 4;		// If BIDING_ZONE < countdown <= CHECKING_ZONE
+                            // then check each CHECKING_PERIOD if placing a bid is necessary
+var CHECKING_PERIOD = 300;	// Time in ms, (cf CHECKING_ZONE desc)
 
 /*  Variable Global */
 var intervalID = null
@@ -22,7 +26,6 @@ function _getAuctionIdFromUrl() {
 
 /*
  * @Desc: Get countdown value
- *        Thanks to oopad for getCountdownValue func
  */
 function _getCountDownValue(b) {
 	var f = jQuery("#Auction_" + b + " .Timer");
@@ -31,7 +34,14 @@ function _getCountDownValue(b) {
 }
 
 function _log(b, msg) {
-	console.log("bid[" + b + "]: " + msg);
+	var d = new Date();
+	var n = d.toLocaleString();
+	console.log(n + " bid[" + b + "]: " + msg);
+}
+
+function _getBidHolder(b) {
+	var winner = jQuery("#Auction_" + b).find(".CurrentWinnerPublicName").html();
+	return winner;
 }
 
 /*
@@ -39,33 +49,58 @@ function _log(b, msg) {
  */
 function _autoBid(b) {
 	var count_down = _getCountDownValue(auctionId);
-	var winner = jQuery("#Auction_" + b).find(".CurrentWinnerPublicName").html();
-	_log(b, winner + " holds the bid at - " + count_down );
+	var winner = _getBidHolder(b);
+	_log(b, winner + " holds the bid at " + count_down );
 	if (count_down == null)
 	{
-		// Case: Bid is paused
-		_log(b, "Midnight or sold");
+        if (winner == "--")
+        {
+            _log(b, "Auction closed, won by " + winner);
+        }
+        else
+        {
+            // Auction is paused at midnight
+            _log(b, "Auction paused");
+        }
 	}
-	else if (count_down <= MONITORING_ZONE)
+	else if (count_down <= CHECKING_ZONE)
 	{
-		// Case: Watch carefully (check each MONITORING_ZONE)
-		if (intervalID == null) {
-            intervalID = setInterval(function(){ _autoBid(auctionId) }, MONITORING_PERIOD);
+		// In the checking zone
+		if (intervalID == null)
+        {
+            intervalID = setInterval(function(){ _autoBid(auctionId) }, CHECKING_PERIOD);
 		}
 		if (count_down <= BIDING_ZONE && winner != PublicName) {
-			// Place bid :placeBid(58237)
-		    _log(b, "A bid was placed");
-			window.location.reload();
+            if (winner == "--")
+            {
+                //Bid hasn't begun
+                var reload_countdown = 15 * 1000;
+                _log(b, "Reload windows in " + reload_countdown + " ms");
+                setTimeout(function(){ window.location.reload(); }, reload_countdown );
+                clearInterval(intervalID);
+                intervalID = null;
+            }
+            else
+            {
+			    placeBid(b);
+		        _log(b, "A bid was placed");
+			    window.location.reload();
+            }
 		}
 	}
 	else
 	{
-		// Case: Wait until the Watch carefully timezone
-		var reload_countdown = (count_down - MONITORING_ZONE) * 1000;
-		_log(b, "Reload windows in " + reload_countdown + " ms");
-		setTimeout(function(){ window.location.reload(); }, reload_countdown );
-		clearInterval(intervalID);
-		intervalID = null
+        if (winner == "--") {
+            //Bid hasn't begun
+		    var reload_countdown = 30 * 1000;
+        } else {
+		    // Case: Wait until the check actively zone
+		    var reload_countdown = (count_down - CHECKING_ZONE) * 1000;
+		    clearInterval(intervalID);
+		    intervalID = null;
+        }
+        _log(b, "Reload windows in " + reload_countdown + " ms");
+        setTimeout(function(){ window.location.reload(); }, reload_countdown );
 	}
 }
 
